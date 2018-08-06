94a2231b30147c101add21c9d7ffc94ac9cd41bc
unknown <omar.abdelbadie1@gmail.com>
2018-08-05 19:40:31 -0700

    post and get test user info

--- src/api/users.js
+++ src/api/users.js
@@ -2,14 +2,20 @@
 const axios = require("axios");
 
 export const testFunc = () => {
-  axios
-    .post("https://chatbackfullstacktest.herokuapp.com/users", {
-      gender: "Male",
-      preferredGenderOfPartner: "Male",
-      age: 23,
-      username: "yyy",
-      uid: "13a"
-    })
-    .then(res => console.log(res))
-    .catch(e => console.log(e));
+  return axios.post("https://chatbackfullstacktest.herokuapp.com/users", {
+    id: 80,
+    gender: "Male",
+    preferredGenderOfPartner: "Male",
+    age: 23,
+    username: "yyy",
+    uid: "13a"
+  });
+};
+
+export const testGet = () => {
+  return axios.get("https://chatbackfullstacktest.herokuapp.com/users", {
+    params: {
+      uid: 124
+    }
+  });
 };
--- src/components/account-components/Account.js
+++ src/components/account-components/Account.js
@@ -5,6 +5,7 @@ import "../App.css";
 import AuthUserContext from "../AuthUserContext";
 import { auth } from "../../firebase";
 import BioForm from "./BioForm.js";
+import { users } from "../../api";
 
 const AccountPage = () => {
   return (
@@ -18,6 +19,7 @@ const AccountPage = () => {
             <h2>Change Password</h2>
             <PasswordChangeForm />
             <BioForm userId={authUser.uid} />
+            <APItest />
             <h3>
               TODO: Make these options dropdown panels; User picture; User
               'about me'
@@ -76,6 +78,40 @@ class UserNameForm extends React.Component {
   }
 }
 
+class APItest extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  //   // state = {};
+  // }
+  testPost = event => {
+    users
+      .testFunc()
+      .then(res => console.log(res))
+      .catch(e => console.log(e));
+  };
+
+  testGet = event => {
+    users
+      .testGet()
+      .then(res => console.log(res))
+      .catch(e => console.log(e));
+  };
+
+  render() {
+    return (
+      <div>
+        <h3>Test API backend calls:</h3>
+        <button value="POST" onClick={this.testPost}>
+          POST{" "}
+        </button>
+        <button value="GET" onClick={users.testGet}>
+          {" "}
+          GET{" "}
+        </button>
+      </div>
+    );
+  }
+}
 const authCondition = authUser => !!authUser;
 
 export default withAuthorization(authCondition)(AccountPage);
