d1eedf47c8119fe30d16f85107a314bd3efb87de
Kyler Griffith <lockheedmartian@gmail.com>
2018-08-20 21:45:11 -0700

    updated UX

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -228,7 +228,7 @@ class Chat extends Component {
     user: {},
     activePrompts: ["concerns"],
     finishedPrompts: [],
-    activeResponses: ["concernsResponse"],
+    activeResponses: [],
     finishedResponses: [],
     selectedButtons: []
   };
